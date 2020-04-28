a7add8729f7134e584c8eecfc7a6293ae57ee412
yzernik <yzernik@gmail.com>
2020-04-27 19:53:09 -0700

    First attempt at squeak implementation, test not complete.

---
+++ src/main/java/io/github/yzernik/core/Squeak.java
@@ -0,0 +1,258 @@
+package io.github.yzernik.core;
+
+import org.bitcoinj.core.*;
+import org.bitcoinj.script.Script;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+public class Squeak extends Message {
+
+    private static final Logger log = LoggerFactory.getLogger(Squeak.class);
+
+    /** How many bytes are required to represent a block header WITHOUT the trailing 00 length byte. */
+    public static final int HEADER_SIZE = 80;
+
+    static final long ALLOWED_TIME_DRIFT = 2 * 60 * 60; // Same value as Bitcoin Core.
+
+    /**
+     * A constant shared by the entire network: how large in bytes a block is allowed to be. One day we may have to
+     * upgrade everyone to change this, so Bitcoin can continue to grow. For now it exists as an anti-DoS measure to
+     * avoid somebody creating a titanically huge but valid block and forcing everyone to download/store it forever.
+     */
+    public static final int MAX_BLOCK_SIZE = 1 * 1000 * 1000;
+    /**
+     * A "sigop" is a signature verification operation. Because they're expensive we also impose a separate limit on
+     * the number in a block to prevent somebody mining a huge block that has way more sigops than normal, so is very
+     * expensive/slow to verify.
+     */
+    public static final int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE / 50;
+
+    /** A value for difficultyTarget (nBits) that allows half of all possible hash solutions. Used in unit testing. */
+    public static final long EASIEST_DIFFICULTY_TARGET = 0x207fFFFFL;
+
+    /** Value to use if the block height is unknown */
+    public static final int BLOCK_HEIGHT_UNKNOWN = -1;
+    /** Height of the first block */
+    public static final int BLOCK_HEIGHT_GENESIS = 0;
+
+    public static final long BLOCK_VERSION_GENESIS = 1;
+    /** Block version introduced in BIP 34: Height in coinbase */
+    public static final long BLOCK_VERSION_BIP34 = 2;
+    /** Block version introduced in BIP 66: Strict DER signatures */
+    public static final long BLOCK_VERSION_BIP66 = 3;
+    /** Block version introduced in BIP 65: OP_CHECKLOCKTIMEVERIFY */
+    public static final long BLOCK_VERSION_BIP65 = 4;
+
+    // Fields defined as part of the protocol format.
+    private long version;
+    private Sha256Hash hashEncContent;
+    private Sha256Hash hashReplySqk;
+    private Sha256Hash hashBlock;
+    private long nBlockHeight;
+
+    // A transaction output has a script used for authenticating that the redeemer is allowed to spend
+    // this output.
+    private byte[] scriptBytes;
+    // The script bytes are parsed and turned into a Script on demand.
+    private Script scriptPubKey;
+    // These fields are not Bitcoin serialized. They are used for tracking purposes in our wallet
+    // only. If set to true, this output is counted towards our balance. If false and spentBy is null the tx output
+    // was owned by us and was sent to somebody else. If false and spentBy is set it means this output was owned by
+    // us and used in one of our own transactions (eg, because it is a change output).
+    private int scriptLen;
+
+    private Sha256Hash hashDataKey;
+    private VCH_IV vchIv;
+
+    private long ntime;
+    private long nNonce;
+    // END OF HEADER
+
+
+    // TODO: Get rid of all the direct accesses to this field. It's a long-since unnecessary holdover from the Dalvik days.
+    /** If null, it means this object holds only the headers. */
+    @Nullable
+    EncContent encContent;
+
+    @Nullable
+    Script scriptSig;
+
+    @Nullable
+    VCH_DATA_KEY vchDataKey;
+
+    /** Stores the hash of the block. If null, getHash() will recalculate it. */
+    private Sha256Hash hash;
+
+    protected boolean headerBytesValid;
+    protected boolean contentBytesValid;
+
+    // Blocks can be encoded in a way that will use more bytes than is optimal (due to VarInts having multiple encodings)
+    // MAX_BLOCK_SIZE must be compared to the optimal encoding, not the actual encoding, so when parsing, we keep track
+    // of the size of the ideal encoding in addition to the actual message size (which Message needs)
+    protected int optimalEncodingMessageSize;
+
+
+    @Override
+    protected void parse() throws ProtocolException {
+        // header
+        cursor = offset;
+        version = readUint32();
+        hashEncContent = readHash();
+        hashReplySqk = readHash();
+        hashBlock = readHash();
+        nBlockHeight = readUint32();
+
+        // parse the script pubkey
+        scriptLen = (int) readVarInt();
+        length = cursor - offset + scriptLen;
+        scriptBytes = readBytes(scriptLen);
+
+        hashDataKey = readHash();
+        vchIv = null;
+        ntime = readUint32();
+        nNonce = readUint32();
+        // Get the hash
+        hash = Sha256Hash.wrapReversed(Sha256Hash.hashTwice(payload, offset, cursor - offset));
+        headerBytesValid = serializer.isParseRetainMode();
+
+        // transactions
+        parseContent(offset + HEADER_SIZE);
+        length = cursor - offset;
+    }
+
+
+    /**
+     * Parse content from the squeak.
+     *
+     * @param contentOffset Offset of the transactions within the squeak.
+     */
+    protected void parseContent(final int contentOffset) throws ProtocolException {
+        cursor = contentOffset;
+        optimalEncodingMessageSize = HEADER_SIZE;
+        if (payload.length == cursor) {
+            // This message is just a header, it has no content.
+            contentBytesValid = false;
+            return;
+        }
+
+        // Get the enc content.
+        encContent = new EncContent(params, payload, cursor, this, serializer, UNKNOWN_LENGTH, null);
+        cursor += encContent.getMessageSize();
+
+        // Get the script sig.
+        int scriptLen = (int) readVarInt();
+        length = cursor - offset + scriptLen + 4;
+        scriptBytes = readBytes(scriptLen);
+        scriptSig = new Script(scriptBytes);
+
+        // Get the data key.
+        vchDataKey = new VCH_DATA_KEY(params, payload, cursor, this, serializer, UNKNOWN_LENGTH, null);
+        cursor += vchDataKey.getMessageSize();
+
+        contentBytesValid = serializer.isParseRetainMode();
+    }
+
+
+    public static class EncContent extends ChildMessage {
+        private static int ENC_CONTENT_LENGTH = 1136;
+        private byte[] bytes;
+
+        /**
+         * Creates a transaction by reading payload starting from offset bytes in. Length of a transaction is fixed.
+         * @param params NetworkParameters object.
+         * @param payload Bitcoin protocol formatted byte array containing message content.
+         * @param offset The location of the first payload byte within the array.
+         * @param parent The parent of the transaction.
+         * @param setSerializer The serializer to use for this transaction.
+         * @param length The length of message if known.  Usually this is provided when deserializing of the wire
+         * as the length will be provided as part of the header.  If unknown then set to Message.UNKNOWN_LENGTH
+         * @param hashFromHeader Used by BitcoinSerializer. The serializer has to calculate a hash for checksumming so to
+         * avoid wasting the considerable effort a set method is provided so the serializer can set it. No verification
+         * is performed on this hash.
+         * @throws ProtocolException
+         */
+        public EncContent(NetworkParameters params, byte[] payload, int offset, @Nullable Message parent,
+                           MessageSerializer setSerializer, int length, @Nullable byte[] hashFromHeader) throws ProtocolException {
+            super(params, payload, offset, parent, setSerializer, length);
+            /* TODO: cache the content hash
+            if (hashFromHeader != null) {
+                cachedWTxId = Sha256Hash.wrapReversed(hashFromHeader);
+                if (!hasWitnesses())
+                    cachedTxId = cachedWTxId;
+            }
+            */
+        }
+
+        @Override
+        protected void parse() throws ProtocolException {
+            bytes = readBytes(ENC_CONTENT_LENGTH);
+        }
+
+        protected void bitcoinSerializeToStream(OutputStream stream) throws IOException {
+            assert bytes.length == ENC_CONTENT_LENGTH;
+            stream.write(bytes);
+        }
+    }
+
+    public static class VCH_IV extends ChildMessage {
+        private static int CIPHER_BLOCK_LENGTH = 16;
+        private byte[] bytes;
+
+        @Override
+        protected void parse() throws ProtocolException {
+            bytes = readBytes(CIPHER_BLOCK_LENGTH);
+        }
+
+        protected void bitcoinSerializeToStream(OutputStream stream) throws IOException {
+            assert bytes.length == CIPHER_BLOCK_LENGTH;
+            stream.write(bytes);
+        }
+    }
+
+
+    public static class VCH_DATA_KEY extends ChildMessage {
+        private static int DATA_KEY_LENGTH = 32;
+        private byte[] bytes;
+
+        /**
+         * Creates a transaction by reading payload starting from offset bytes in. Length of a transaction is fixed.
+         * @param params NetworkParameters object.
+         * @param payload Bitcoin protocol formatted byte array containing message content.
+         * @param offset The location of the first payload byte within the array.
+         * @param parent The parent of the transaction.
+         * @param setSerializer The serializer to use for this transaction.
+         * @param length The length of message if known.  Usually this is provided when deserializing of the wire
+         * as the length will be provided as part of the header.  If unknown then set to Message.UNKNOWN_LENGTH
+         * @param hashFromHeader Used by BitcoinSerializer. The serializer has to calculate a hash for checksumming so to
+         * avoid wasting the considerable effort a set method is provided so the serializer can set it. No verification
+         * is performed on this hash.
+         * @throws ProtocolException
+         */
+        public VCH_DATA_KEY(NetworkParameters params, byte[] payload, int offset, @Nullable Message parent,
+                          MessageSerializer setSerializer, int length, @Nullable byte[] hashFromHeader) throws ProtocolException {
+            super(params, payload, offset, parent, setSerializer, length);
+            /* TODO: cache the data key hash
+            if (hashFromHeader != null) {
+                cachedWTxId = Sha256Hash.wrapReversed(hashFromHeader);
+                if (!hasWitnesses())
+                    cachedTxId = cachedWTxId;
+            }
+            */
+        }
+
+        @Override
+        protected void parse() throws ProtocolException {
+            bytes = readBytes(DATA_KEY_LENGTH);
+        }
+
+        protected void bitcoinSerializeToStream(OutputStream stream) throws IOException {
+            assert bytes.length == DATA_KEY_LENGTH;
+            stream.write(bytes);
+        }
+    }
+}
