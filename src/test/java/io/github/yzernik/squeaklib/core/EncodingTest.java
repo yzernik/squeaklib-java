5de66f01ff829a1aa26859cf9dfc5621abe39b62
yzernik <yzernik@gmail.com>
2020-05-20 17:59:13 -0700

    Throw exception when encoded string bytes are longer than maximum allowed.

--- src/test/java/io/github/yzernik/squeaklib/core/EncodingTest.java
+++ src/test/java/io/github/yzernik/squeaklib/core/EncodingTest.java
@@ -12,7 +12,32 @@ public class EncodingTest {
         byte[] encodedMsg = Encoding.encodeMessage(message);
         String decodedMsg = Encoding.decodeMessage(encodedMsg);
 
+        assertEquals(encodedMsg.length, Encoding.CONTENT_LENGTH);
         assertEquals(decodedMsg, message);
+    }
+
+    @Test
+    public void testEncodeDecodeMaximumLength() throws Exception {
+        String message = createStringOfLength('*', Encoding.CONTENT_LENGTH);
+        byte[] encodedMsg = Encoding.encodeMessage(message);
+        String decodedMsg = Encoding.decodeMessage(encodedMsg);
+
+        assertEquals(encodedMsg.length, Encoding.CONTENT_LENGTH);
+        assertEquals(decodedMsg, message);
+    }
+
+    @Test(expected = EncodingException.class)
+    public void testEncodeDecodeAboveMaximumLength() throws Exception {
+        String message = createStringOfLength('*', Encoding.CONTENT_LENGTH + 1);
+        Encoding.encodeMessage(message);
+    }
+
+    private static String createStringOfLength(char c, int n){
+        //create new string from char array of required size
+        String str = new String(new char[n]);
+        //replace all NUL chars '\0' with specified char
+        str = str.replace('\0', c);
+        return str;
     }
 
 }
